be039c3aed26eb89bbd99e1e9cdadda8a0097870
kapade-lokesh <lkkapade@gmail.com>
2025-06-24 21:39:04 +0530

    userRoutes add to app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from "express";
-
+import userRoutes from "./routes/user.route";
 const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("hellow");
+});
+app.use("/user", userRoutes);
 
 export default app;
