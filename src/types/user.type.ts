2039d6dab7d6fc1a076ec54e914fa2a3f87da309
kapade-lokesh <lkkapade@gmail.com>
2025-06-24 21:33:12 +0530

    added user types

---
+++ src/types/user.type.ts
@@ -0,0 +1,10 @@
+import { Document } from "mongoose";
+export interface IUser extends Document {
+  name?: string;
+  email: string;
+  password?: string; // optional for OAuth users
+  provider: "credentials" | "google" | "github"; // extendable
+  providerId?: string; // e.g., GitHub user ID
+  avatar?: string;
+}
+
